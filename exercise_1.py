02f92358eea95344f56c01854b0ec92c7fcb7a2b
Juan Bull <53854655+juanbull@users.noreply.github.com>
2019-08-07 11:38:27 +0200

    Add files via upload

---
+++ exercise_1.py
@@ -0,0 +1,28 @@
+print ("Hello World 1")
+print ("Hello World 2")
+print ("Hello World 3")
+print ("Hello World 4")
+print ("Stop is enough")
+c="Esto es una frase demasiado larga en español"
+print(c)
+a=2.5
+c=int(a)
+b=c+c+c
+print(b)
+horas = input ("¿Cuántas horas a la semana trabajaste?")
+precio = input ("¿Qué precio te pagan por tu categoría?")
+salario = float(horas) * float(precio)
+print("Esta semana has ganado:",salario)
+print("......................................")
+x = 5
+if x> 2: 
+	print('Más grande que 2')
+	print('Todavía más grande')
+print ('Hecho con el 2')
+i=int(1)
+for i in range(5)
+	print(i)
+	if i>2: 
+	   print ('Brigger than 2')
+	print('Done with i', i) 
+print ('All Done')
